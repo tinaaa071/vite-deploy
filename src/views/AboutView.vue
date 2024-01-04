a3ceb2d28b4211fd009ceb7fc4f5c3a39cc3e9c7
Tina <tina.chen@u-ark.com>
2024-01-04 18:04:39 +0800

    new

---
+++ src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {}
+    }
+  },
+  mounted () {
+    // 載入環境變數
+    console.log(import.meta.env.VITE_TEXT)
+    // 取出路徑
+    const url = import.meta.env.VITE_PATH
+    // 取得遠端陣列
+    this.$http.get(url)
+    .then((res) => {
+      console.log(res)
+      this.data = res.data.results[0]
+      console.log(this.data)
+    })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
