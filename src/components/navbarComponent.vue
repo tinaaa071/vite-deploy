c257a960518cd837016cc40e10bb87a288058616
Tina <tina.chen@u-ark.com>
2024-01-05 14:36:25 +0800

    title

--- src/components/navbarComponent.vue
+++ src/components/navbarComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar bg-light">
         <div class="container-fluid">
-        <span class="navbar-brand" href="#">香香麵攤</span>
+        <span class="navbar-brand" href="#">香香餅乾店</span>
         <button type="button" class="btn nav-link">
             購物車
             <span class="badge rounded-pill bg-danger text-white">{{ cart.length }}</span>
